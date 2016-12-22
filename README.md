47b46afc73cb266e5fdb1fa2a041f344f20ed531
Daniel Rohe <danielrohe@users.noreply.github.com>
2016-12-22 10:22:13 +0100

    feature/spring boot restructuring (#3)
    * restructured based on spring-boot guidelines
    
    * corrected imports

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Spring-boot-etcd is a [Spring Boot](http://projects.spring.io/spring-boot/) libr
 
 ###Inspiration
 
-We created spring-boot-etcd after discovering that current implementations of the etcd client API don’t provide an automatic update mechanism, or use other libraries like [Netty](http://netty.io/) to communicate with the etcd cluster. Also, these implementations are not compatible the version of Netty used by the Cassandra driver.
+We created zalando-boot-etcd after discovering that current implementations of the etcd client API don’t provide an automatic update mechanism, or use other libraries like [Netty](http://netty.io/) to communicate with the etcd cluster. Also, these implementations are not compatible the version of Netty used by the Cassandra driver.
 
 ### Getting Started
 
@@ -21,11 +21,11 @@ Use the following Maven dependency to add this library:
 
     <dependency>
       <groupId>org.zalando</groupId>
-      <artifactId>spring-boot-etcd</artifactId>
-      <version>1.4</version>
+      <artifactId>zalando-boot-etcd-starter</artifactId>
+      <version>RELEASE</version>
     </dependency>
 
-You can find the latest version at [Maven Central](http://search.maven.org/#search|ga|1|g%3A%22org.zalando%22%20a%3A%22spring-boot-etcd%22).
+You can find the latest version at [Maven Central](http://search.maven.org/#search|ga|1|g%3A%22org.zalando%22%20a%3A%22zalando-boot-etcd-starter%22).
 
 ###Configuration
 
@@ -72,20 +72,6 @@ Write a small Spring Boot application and add this project as a dependency. Use 
 
 ## Contributing
 
-This project accepts contributions from the open-source community, including bug fixes and feature adds. Before submitting a major change or PR, please open an issue describing the change or addition you would like to make.
+This project accepts contributions from the open-source community, including bug fixes and feature adds.
 
-## License
-
-Copyright 2015 Zalando SE
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Before making a contribution, please let us know by posting a comment to the relevant issue. And if you would like to propose a new feature, do start a new issue explaining the feature you’d like to contribute.
---
+++ zalando-boot-etcd-autoconfigure/src/main/java/org/zalando/boot/etcd/autoconfigure/EtcdClientAutoConfiguration.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2015 Zalando SE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zalando.boot.etcd.autoconfigure;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.zalando.boot.etcd.EtcdClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * The auto configuration of the etcd client usign either an initial list of
+ * locations or a service name for DNS discovery.
+ */
+@Configuration
+@EnableConfigurationProperties(EtcdClientProperties.class)
+@ConditionalOnClass(ObjectMapper.class)
+@ConditionalOnProperty(prefix = "zalando.etcd", name = "enabled", matchIfMissing = true)
+public class EtcdClientAutoConfiguration {
+
+	@Configuration
+	@ConditionalOnProperty(prefix = "zalando.etcd", name = "location")
+	protected static class StaticDiscoveryConfiguration {
+
+		@Autowired
+		private EtcdClientProperties properties = new EtcdClientProperties();
+
+		@Bean
+		public EtcdClient etcdService() {
+			EtcdClient client = new EtcdClient(properties.getLocation());
+
+			client.setRetryCount(properties.getRetryCount());
+			client.setRetryDuration(properties.getRetryDuration());
+			client.setLocationUpdaterEnabled(properties.isUpdateLocations());
+
+			SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+			requestFactory.setConnectTimeout(properties.getConnectTimeout());
+			requestFactory.setReadTimeout(properties.getReadTimeout());
+			client.setRequestFactory(requestFactory);
+
+			return client;
+		}
+	}
+
+	@Configuration
+	@ConditionalOnProperty(prefix = "zalando.etcd", name = "serviceName")
+	protected static class DnsDiscoveryConfiguration {
+
+		@Autowired
+		private EtcdClientProperties properties = new EtcdClientProperties();
+
+		private List<String> discoverNodes(String serviceName) throws NamingException {
+			List<String> locations = new ArrayList<>();
+
+			Hashtable<String, String> env = new Hashtable<String, String>();
+			env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+			env.put("java.naming.provider.url", "dns:");
+
+			DirContext context = new InitialDirContext(env);
+			Attributes attributes = context.getAttributes(serviceName, new String[] { "SRV" });
+			for (NamingEnumeration<? extends Attribute> records = attributes.getAll(); records.hasMore();) {
+				Attribute record = records.next();
+				NamingEnumeration<String> values = (NamingEnumeration<String>) record.getAll();
+				while (values.hasMore()) {
+					String dns = values.next();
+					String[] split = dns.split(" ");
+					String host = split[3];
+					if (host.endsWith(".")) {
+						host = host.substring(0, host.length() - 1);
+					}
+
+					String location = "http://" + host + ":2379";
+					locations.add(location);
+				}
+			}
+			return locations;
+		}
+
+		@Bean
+		public EtcdClient etcdService() throws NamingException {
+			List<String> locations = discoverNodes("_etcd-server._tcp." + properties.getServiceName());
+
+			EtcdClient client = new EtcdClient(locations.get(0));
+
+			client.setRetryCount(properties.getRetryCount());
+			client.setRetryDuration(properties.getRetryDuration());
+			client.setLocationUpdaterEnabled(properties.isUpdateLocations());
+
+			SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+			requestFactory.setConnectTimeout(properties.getConnectTimeout());
+			requestFactory.setReadTimeout(properties.getReadTimeout());
+			client.setRequestFactory(requestFactory);
+
+			return client;
+		}
+	}
+}
